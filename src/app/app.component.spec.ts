0715affa7d92c609614f17ee2b0c989ab9daf634
Oscar Valdes <oscar94valdes@gmail.com>
2018-10-14 23:27:06 -0400

    add tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,46 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { routes } from './app.routing.module';
 import { AppComponent } from './app.component';
+import {Router} from '@angular/router';
+import {Location} from '@angular/common';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {HomeModule} from './home/home.module';
 
 describe('AppComponent', () => {
+  let router: Router;
+  let location: Location;
+  let fixture;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes(routes), HomeModule],
       declarations: [
-        AppComponent
+        AppComponent,
+        PageNotFoundComponent
       ],
     }).compileComponents();
+
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+    fixture = TestBed.createComponent(AppComponent);
+    router.initialNavigation();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  fit('should create the app', () => {
+    fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular6-Sample-App'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  fit(`should have as title 'Everything Fortnite'`, () => {
+    fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular6-Sample-App');
+    expect(app.title).toEqual('Everything Fortnite');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular6-Sample-App!');
--- src/app/store-info/store/store.service.spec.ts
+++ src/app/store-info/store/store.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
 import { StoreService } from './store.service';
 
 describe('StoreService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: StoreService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [],
+      providers: [
+        StoreService
+      ]
+    });
+    service = TestBed.get(StoreService);
+  });
 
   it('should be created', () => {
-    const service: StoreService = TestBed.get(StoreService);
     expect(service).toBeTruthy();
   });
 });
